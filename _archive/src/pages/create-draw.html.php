c9613dc1508f8b2f8536915567e48873fb1411b5
thewholelo <lo@thewholelo.com>
2018-01-18 10:26:40 +1000

    initial add and commit

---
+++ _archive/src/pages/create-draw.html.php
@@ -0,0 +1,26 @@
+<div class="grid-container">
+    <div class="grid-x grid-margin-x grid-padding-y">
+
+        <div class="large-12 cell">
+            <h4>CREATE A DRAW</h4>
+        </div>
+    </div>
+
+    <div class="grid-x grid-margin-x">
+        <div class="large-12 cell">
+            <div class="">
+                <p>Use the following form to create a new draw.</p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="grid-x grid-margin-x">
+        <div class="large-12 cell">
+            <div class="stacked-for-small">
+                <a href="" class="button expanded">Create Draw</a>
+            </div>
+
+        </div>
+    </div>
+</div>
